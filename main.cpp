4d0c57078db1cb18a10766909ca97475fc9c276c
Ancordss <maradiaga.l.james@gmail.com>
2022-03-30 22:46:35 -0600

    main creado

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+int cont = 0;
+
+void menu();/*
+void crear(); // javier
+void leer();	//maria
+void actualizar(); //jonathan
+void delete_line(); //carlos
+void readforiginal();//james
+void diccionario();//james
+void lecturaCodigo();//james
+
+*/
+
+main(){
+  menu();
+
+
+
+
+}
+void menu(){
+    system("cls");
+    std::cout << "@@@@@@@  @@@@@@@    @@@@@@   @@@@@@@   @@@  @@@   @@@@@@@  @@@@@@@   @@@@@@   @@@@@@@  " << '\n';
+    std::cout << "@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@  @@@  @@@@@@@@  @@@@@@@  @@@@@@@@  @@@@@@@@  " << '\n';
+    std::cout << "  @@!    @@!  @@@  @@!  @@@  @@!  @@@  @@!  @@@  !@@         @@!    @@!  @@@  @@!  @@@  " << '\n';
+    std::cout << "  !@!    !@!  @!@  !@!  @!@  !@!  @!@  !@!  @!@  !@!         !@!    !@!  @!@  !@!  @!@ " << '\n';
+    std::cout << "  @!!    @!@!!@!   @!@!@!@!  @!@  !@!  @!@  !@!  !@!         @!!    @!@  !@!  @!@!!@!  " << '\n';
+    std::cout << "  !!!    !!@!@!    !!!@!!!!  !@!  !!!  !@!  !!!  !!!         !!!    !@!  !!!  !!@!@!" << '\n';
+    std::cout << "  !!:    !!: :!!   !!:  !!!  !!:  !!!  !!:  !!!  :!!         !!:    !!:  !!!  !!: :!!  " << '\n';
+    std::cout << "  :!:    :!:  !:!  :!:  !:!  :!:  !:!  :!:  !:!  :!:         :!:    :!:  !:!  :!:  !:! " << '\n';
+    std::cout << "   ::    ::   :::  ::   :::   :::: ::  ::::: ::   ::: :::     ::    ::::: ::  ::   ::: " << '\n';
+    std::cout << "   :      :   : :   :   : :  :: :  :    : :  :    :: :: :     :      : :  :    :   : :  " << '\n';
+
+
+
+/*
+  switch(cont)
+	{
+		case 1: crear();		 		;break; //javier
+ 		case 2: leer();					;break; //maria
+ 		case 3: actualizar();	 		;break; //jonathan
+ 		case 4: delete_line();	 		;break; //carlos
+ 		case 5: lecturaCodigo();		;break; //james
+ 		case 6: exit(-1);				;break;
+		default: MessageBox( 0, "Hello World!", "Greetings", 0 );;
+	}*/
+
+
+
+
+}
