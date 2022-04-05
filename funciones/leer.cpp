fde48eddae6bf20eafd378af2fa7d0f0d024a1cb
Rita Reyes <rita.303015@gmail.com>
2022-04-04 21:11:05 -0600

    agregué leer

---
+++ funciones/leer.cpp
@@ -0,0 +1,40 @@
+#include <iomanip> // se agrego libreria para tabular el ID y el NO
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include "crear.cpp"
+
+
+using namespace std;
+
+void leer(){
+		system("cls");
+		FILE* archivo = fopen(n_archivo,"rb");
+		if (!archivo){
+			archivo = fopen(n_archivo,"w+b");
+		}
+		Traductor traductor;
+		int id=0;
+		fread(&traductor,sizeof(Traductor),1,archivo);
+		cout<<"--------------------"<<endl;
+		cout<<"\t Listado de Palabras con su funcion"<<endl;
+		do{
+		cout<<"--------------------"<<endl;
+
+		cout<<stew(10);
+    cout<<"Id: "<<id;
+    cout<<stew(20);
+		cout<<"No. "<<traductor.num<<endl;
+		cout<<"Palabra en Ingles: "<<traductor.in<<endl;
+		cout<<"     Palabra en Espanol: "<<traductor.esp<<endl;
+		cout<<"                  Funcionalidad:                 "<<traductor.fun<<endl;
+		fread(&traductor,sizeof(Traductor),1,archivo);
+		id+=1;
+		}while(feof(archivo)==0);
+		fclose(archivo);
+		cout<<endl;
+
+		//readforiginal();
+		system("pause");
+		menu();
+}
