7060c8906231e7897bde899201a9c4bd27f79e6b
Ancordss <maradiaga.l.james@gmail.com>
2022-04-04 22:17:54 -0600

    error en leer

--- main.cpp
+++ main.cpp
@@ -11,17 +11,14 @@ por el momento solo lleva esto ya que no se ah terminado todas las funciones
 using namespace std;
 
 
-
-
-
 int cont = 0;
 
 void menu();
-void crear(); 
-void leer();	
+void crear();
+void leer();
 //void actualizar();
-void eliminar_linea(); 
-void R_i();
+void eliminar_linea();
+void R_in();
 
 
 
@@ -32,9 +29,10 @@ main(){
   menu();
 
 
-
-
 }
+
+
+
 void menu(){
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE) ;
     system("cls");
@@ -50,18 +48,24 @@ void menu(){
     SetConsoleTextAttribute (hConsole, 9);std::cout << "   ::    ::   :::  ::   :::   :::: ::  ::::: ::   ::: :::     ::    ::::: ::  ::   ::: " << '\n';
     SetConsoleTextAttribute (hConsole, 10);std::cout << "   :      :   : :   :   : :  :: :  :    : :  :    :: :: :     :      : :  :    :   : :  " << '\n';
     SetConsoleTextAttribute (hConsole, 11);
-
-/*
+    system("pause");
+    system("cls");
+    std::cout << "ingrese 1 para crear" << '\n';
+    std::cout << "ingrese 2 para leer " << '\n';
+    std::cout << "ingrese 3 para actualizar" << '\n';
+    std::cout << "ingrese 4 para borrar linea" << '\n';
+    std::cout << "ingrese 5 para traducir" << '\n';
+    std::cout << "ingresa 6 para salir" << '\n';
   switch(cont)
 	{
 		case 1: crear();		 		;break; //javier
  		case 2: leer();					;break; //maria
- 		case 3: actualizar();	 		;break; //jonathan
- 		case 4: delete_line();	 		;break; //carlos
- 		case 5: lecturaCodigo();		;break; //james
+ 	//	case 3: actualizar();	 		;break; //jonathan
+ 		case 4: eliminar_linea();	 		;break; //carlos
+ 		case 5: R_in();		;break; //james
  		case 6: exit(-1);				;break;
-		default: MessageBox( 0, "Hello World!", "Greetings", 0 );;
-	}*/
+		default: MessageBox( 0, "Hello World!", "Greetings", 0 );
+	}
 
 
 
