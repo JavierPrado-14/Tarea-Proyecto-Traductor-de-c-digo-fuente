4647a3df5ba116bd17ef0636e9872addfbbcb24b
Rita Reyes <rita.303015@gmail.com>
2022-04-04 22:35:52 -0600

    cambios2

--- funciones/leer.cpp
+++ funciones/leer.cpp
@@ -16,18 +16,18 @@ void leer(){
 		Traductor traductor;
 		int id=0;
 		fread(&traductor,sizeof(Traductor),1,archivo);
-		cout<<"--------------------"<<endl;
+		cout<<"---------------------------------------------------------------"<<endl;
 		cout<<"\t Listado de Palabras con su funcion"<<endl;
 		do{
-		cout<<"--------------------"<<endl;
+		cout<<"---------------------------------------------------------------"<<endl;
 
 		cout<<setw(10);
-    cout<<"Id: "<<id;
-    cout<<setw(20);
+        cout<<"Id: "<<id;
+        cout<<setw(20);
 		cout<<"No. "<<traductor.num<<endl;
-		cout<<"Palabra en Ingles: "<<traductor.in<<endl;
-		cout<<"     Palabra en Espanol: "<<traductor.esp<<endl;
-		cout<<"                  Funcionalidad:                 "<<traductor.fun<<endl;
+		cout<<" Palabra en Ingles: "<<traductor.in<<endl;
+		cout<<" Palabra en Espanol: "<<traductor.esp<<endl;
+		cout<<"\t                 Funcionalidad: "<<traductor.fun<<endl;
 		fread(&traductor,sizeof(Traductor),1,archivo);
 		id+=1;
 		}while(feof(archivo)==0);
